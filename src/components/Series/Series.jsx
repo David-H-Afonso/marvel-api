6b39db646ffa492b9f52fe93d8001530c9cc21b5
David Hormiga Afonso <davidhormigafonso@gmail.com>
2023-03-07 20:10:03 +0000

    Create Series base code

--- src/components/Series/Series.jsx
+++ src/components/Series/Series.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { URL_ALL_SERIES } from '../../utils/api_url'
+import SeriesCard from './SeriesCard'
 
 const Series = (props) => {
+	const [series, setSeries] = useState([])
+
+	useEffect(() => {
+		fetchSeries()
+	}, [])
+
+	const fetchSeries = async () => {
+		const response = await fetch(`${URL_ALL_SERIES}&limit=5`).then((res) => res.json())
+		setSeries(response)
+	}
+
 	return (
 		<div>
 			<p>Series</p>
+			{series?.data?.results?.map((serie) => (
+				<SeriesCard serie={serie} key={serie.id} />
+			))}
 		</div>
 	)
 }
