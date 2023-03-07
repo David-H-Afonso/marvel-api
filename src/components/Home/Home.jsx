1b038e71bdd4474451b3531be923227aa14af715
David Hormiga Afonso <davidhormigafonso@gmail.com>
2023-03-07 20:11:01 +0000

    Delete unnecesary data home page

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,25 +1,6 @@
-import React, { useState, useEffect, useMemo } from 'react'
-import { URL_ALL_COMICS, URL_ALL_SERIES } from '../../utils/api_url'
+import React from 'react'
 
 const Home = (props) => {
-	const [comics, setComics] = useState([])
-	const [series, setSeries] = useState([])
-
-	useEffect(() => {
-		// fetchComics()
-		// fetchSeries()
-	}, [])
-
-	const fetchComics = async () => {
-		const response = await fetch(`${URL_ALL_COMICS}&limit=2`).then((res) => res.json())
-		setComics(response)
-	}
-
-	const fetchSeries = async () => {
-		const response = await fetch(URL_ALL_SERIES).then((res) => res.json())
-		setSeries(response)
-	}
-
 	return (
 		<div>
 			<p>Home</p>
