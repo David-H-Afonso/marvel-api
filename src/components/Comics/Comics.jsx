1b4fdc17d086f9525013fbe3f25d228aca248019
David Hormiga Afonso <davidhormigafonso@gmail.com>
2023-03-07 20:10:42 +0000

    Create Comics base

--- src/components/Comics/Comics.jsx
+++ src/components/Comics/Comics.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { URL_ALL_COMICS } from '../../utils/api_url'
+import ComicsCard from './ComicsCard'
 
 const Comics = (props) => {
+	const [comics, setComics] = useState([])
+
+	useEffect(() => {
+		fetchComics()
+	}, [])
+
+	const fetchComics = async () => {
+		const response = await fetch(`${URL_ALL_COMICS}&limit=5`).then((res) => res.json())
+		setComics(response)
+	}
+
 	return (
 		<div>
 			<p>Comics</p>
+			{comics?.data?.results?.map((comic) => (
+				<ComicsCard comic={comic} key={comic.id} />
+			))}
 		</div>
 	)
 }
