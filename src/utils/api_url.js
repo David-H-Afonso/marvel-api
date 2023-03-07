cee31a3542358b54be2bb81f0ffd6c69389f797a
David Hormiga Afonso <davidhormigafonso@gmail.com>
2023-03-07 20:01:05 +0000

    Add prettier and API urls

---
+++ src/utils/api_url.js
@@ -0,0 +1,6 @@
+export const URL_ALL_CHARACTERS =
+	'https://gateway.marvel.com:443/v1/public/characters?apikey=f4e4fbed1d6a5a43eb90769e8ac0f261&hash=af88467d91e686c57d240f3d6886cc1b'
+export const URL_ALL_COMICS =
+	'https://gateway.marvel.com:443/v1/public/comics?apikey=f4e4fbed1d6a5a43eb90769e8ac0f261&hash=af88467d91e686c57d240f3d6886cc1b'
+export const URL_ALL_SERIES =
+	'https://gateway.marvel.com:443/v1/public/series?apikey=f4e4fbed1d6a5a43eb90769e8ac0f261&hash=af88467d91e686c57d240f3d6886cc1b'
