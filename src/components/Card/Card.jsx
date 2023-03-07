36f025d25dac9658d73a792da7035f143d70f94a
David Hormiga Afonso <davidhormigafonso@gmail.com>
2023-03-07 21:08:10 +0000

    Create Card component

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './Card.scss'
+
+const Card = (props) => {
+	const { info, title, description } = props
+
+	const cardInfo = {
+		name: title || 'Name not found',
+		description: description || 'Description not found',
+		image: `${info.thumbnail.path}.${info.thumbnail.extension}`.replace('http', 'https'),
+	}
+
+	return (
+		<div className='card' onClick={''}>
+			<img src={cardInfo.image} alt={cardInfo.title} className='infoImage' />
+			<h1 className='infoTitle'>{cardInfo.name}</h1>
+			<p className='infoDescription'>{cardInfo.description}</p>
+		</div>
+	)
+}
+
+export default Card
