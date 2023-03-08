4e33bde5ff3503dd85f2595112f1318e45eaf53e
David Hormiga Afonso <davidhormigafonso@gmail.com>
2023-03-08 16:37:57 +0000

    Create proper Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,21 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { charactersURL, comicsURL, homeURL, seriesURL } from '../../utils/routes'
+import './Header.scss'
 
 const Header = (props) => {
-	return <div>Header</div>
+	return (
+		<div className='header'>
+			<div className='headerFixed'>
+				<div className='headerLinks'>
+					<Link to={homeURL}>Home</Link>
+					<Link to={charactersURL}>Characters</Link>
+					<Link to={comicsURL}>Comics</Link>
+					<Link to={seriesURL}>Series</Link>
+				</div>
+			</div>
+		</div>
+	)
 }
 
 export default Header
