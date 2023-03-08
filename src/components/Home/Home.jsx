dcc24e799791e951f723876899ef2bb60f0c9415
David Hormiga Afonso <davidhormigafonso@gmail.com>
2023-03-08 16:38:26 +0000

    Edit home page

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,20 +1,49 @@
 import React from 'react'
+import HomeLink from './HomeLink'
+import './Home.scss'
+import { charactersURL, comicsURL, seriesURL } from '../../utils/routes'
 
 const Home = (props) => {
+	const homePage = [
+		{
+			id: 0,
+			title: 'Characters',
+			description: 'Check some of the Marvel characters',
+			image: require('../../assets/characters.png'),
+			link: charactersURL,
+		},
+		{
+			id: 1,
+			title: 'Comics',
+			description: 'Check some amazing Marvel comics',
+			image: require('../../assets/comics.jpg'),
+			link: comicsURL,
+		},
+		{
+			id: 2,
+			title: 'Series',
+			description: 'Check some top level Marvel series',
+			image: require('../../assets/series.png'),
+			link: seriesURL,
+		},
+	]
+
 	return (
-		<div>
-			<p>Home</p>
-			<ul>
-				<li>
-					<a href='/characters'>Characters</a>
-				</li>
-				<li>
-					<a href='/comics'>Comics</a>
-				</li>
-				<li>
-					<a href='/series'>Series</a>
-				</li>
-			</ul>
+		<div className='mainWrapper'>
+			<h1>Home Page</h1>
+			<div className='mainHome'>
+				{homePage?.map((homeUrl) => (
+					<HomeLink
+						info={homeUrl}
+						title={homeUrl.title}
+						description={homeUrl.description}
+						image={homeUrl.image}
+						key={homeUrl.id}
+						link={homeUrl.link}
+						isHomePage
+					/>
+				))}
+			</div>
 		</div>
 	)
 }
---
+++ src/components/Home/HomeLink.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Card = (props) => {
+	const { title, description, image, link } = props
+
+	return (
+		<Link to={link} className='homeLink'>
+			<img src={image} alt={title} className='homeImage' />
+			<h1 className={'homeTitle'}>{title}</h1>
+			<p className='homeDescription'>{description}</p>
+		</Link>
+	)
+}
+
+export default Card
